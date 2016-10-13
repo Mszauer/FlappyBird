d1c59c2ba3a387ab96c137bfc6e47b3ca919fa6e
mszauer <mszauer@mail.usf.edu>
2016-10-12 21:17:51 -0700

    initial commit finished jump  and horizontal scrolling

---
+++ Assets/Scipts/Jump.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+    public float impulse = 200.0f;
+    private Rigidbody2D body2D;
+    bool jumping = false;
+
+    void Awake() {
+        body2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.anyKeyDown) {
+            if (!jumping) {
+                jumping = true;
+                body2D.AddForce(new Vector2(0, impulse), ForceMode2D.Impulse);
+            }
+        }
+
+        jumping = false;
+    }
+}
---
+++ Assets/Scipts/Scrolling.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Scrolling : MonoBehaviour {
+    public List<Image> objects;
+    public float moveSpeed = 1.0f;
+    public float deltaY;
+
+    protected List<float> xOffset;
+    void Awake() {
+        xOffset = new List<float>();
+        foreach (Image obj in objects) {
+            xOffset.Add(obj.rectTransform.rect.width / 2);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	    for (int i = 0; i < objects.Count; i++) {
+            //move x pos
+            objects[i].transform.position = new Vector2(objects[i].transform.position.x - moveSpeed*Time.deltaTime, objects[i].transform.position.y);
+            if (objects[i].transform.position.x+xOffset[i] < 0) {
+                    float xPos = Screen.width + xOffset[i];
+                    objects[i].transform.position = new Vector2(xPos, objects[i].transform.position.y);
+                //if cobject child component<reset> != null
+                //objectchild.resetY();
+                //move y
+                //bounds check top
+                //move bottom
+                //bottom bounds check
+                //move y: resetY{+Random.Range(-deltaY,deltaY)};
+            }
+        }
+    }
+}
